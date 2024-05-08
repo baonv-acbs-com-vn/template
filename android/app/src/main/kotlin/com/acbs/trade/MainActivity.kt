192019ed429fd0801f5e188c7536772e9f61ac78
ACBS 2030 <>
2024-05-08 11:35:13 +0700

    + init project

---
+++ android/app/src/main/kotlin/com/acbs/trade/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.acbs.trade.demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:demo/app/app.dart';
+import 'package:demo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
